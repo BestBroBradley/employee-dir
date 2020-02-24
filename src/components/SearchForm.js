70df692802fb2cf82d3d1be5f08f068e0bd411a8
Allison Bradley <allison.bradley@outlook.com>
2020-02-24 14:25:08 -0700

    Added form field

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -3,6 +3,33 @@ import React from "react"
 
 export const SearchForm = () => {
     return (
-    <h2>Search Form</h2>
+        <div className="container">
+            <div className="row">
+            <div className="col-6">
+            <form>
+                <div className="form-group">
+                    <h2>Search by Name</h2>
+                    <label htmlFor="exampleInputEmail1">Name</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    <small id="emailHelp" className="form-text text-muted">Search for a valid name.</small>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </div>
+            </form>
+            </div>
+
+            <div className="col-6">
+            <form>
+                <div className="form-group">
+                    <h2>Search by Department</h2>
+                    <label htmlFor="exampleInputEmail1">Department</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    <small id="emailHelp" className="form-text text-muted">Search for a valid department.</small>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </div>
+            </form>
+            </div>
+            </div>
+        </div>
+
     )
 }
