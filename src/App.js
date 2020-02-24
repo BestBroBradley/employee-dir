6c5a1b080657cd2081ed291206b5aa8288016bf5
Allison Bradley <allison.bradley@outlook.com>
2020-02-24 14:30:41 -0700

    added card

--- src/App.js
+++ src/App.js
@@ -11,7 +11,12 @@ function App() {
     <>
     <Header />
     <SearchForm />
+    <div className="row">
     <EmpCard />
+    <EmpCard />
+    <EmpCard />
+    <EmpCard />
+    </div>
     </>
   );
 }
--- src/components/EmpCard.js
+++ src/components/EmpCard.js
@@ -3,6 +3,14 @@ import React from "react"
 
 export const EmpCard = () => {
     return (
-    <h2>EmpCard</h2>
+        <div className="col-4-lg col-6-small card" style={{width: "18rem"}}>
+            <div className="card-body">
+                <h5 className="card-title">Emp Name</h5>
+                <h6 className="card-subtitle mb-2 text-muted">Department</h6>
+                <br/>
+                <p className="card-text">Email</p>
+                <p className="card-text">Phone</p>
+            </div>
+        </div>
     )
 }
